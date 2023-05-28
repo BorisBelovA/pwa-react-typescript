dedf203ef7ebb33687bac2f25be0a1f69f06fa4a
Belov <belov.boris.a@gmail.com>
2023-05-28 18:13:32 +0300

    restore common-configuration

--- src/api/common-configuration.ts
+++ src/api/common-configuration.ts
@@ -1,19 +1,8 @@
 import axios from 'axios'
-const envURI = {
-  'DEV': 'http://localhost:3000',
-  'PROD': 'http://194.58.109.74:8080'
-}
 
-const getBasURL = (): string => {
-  const env: string | undefined = process.env.REACT_APP_HOST_TYPE
-  if (env && (env === 'DEV' || env === 'PROD')) {
-    return `${envURI[env]}/api/v1`
-  }
-  return `/api/v1`
-}
 // Common configuration for axios instance
 export default axios.create({
-  baseURL: getBasURL(),
+  baseURL: '/api/v1',
   headers: {
     'Content-type': 'application/json',
     // 'Connection': 'keep-alive',
