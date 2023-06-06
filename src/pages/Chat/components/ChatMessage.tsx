4ca1d6012fd8da2b95cc36a2d946bc7a0c67b781
Belov <belov.boris.a@gmail.com>
2023-06-06 11:12:41 +0300

    Chat UI

--- src/pages/Chat/components/ChatMessage.tsx
+++ src/pages/Chat/components/ChatMessage.tsx
@@ -2,7 +2,8 @@ import { Box, useTheme } from '@mui/material'
 import { getMessageTime } from 'src/utils/date-time'
 import styles from './ChatMessage.module.scss'
 import DoneAllIcon from '@mui/icons-material/DoneAll'
-import { Message } from 'models'
+import { type Message } from 'models'
+import { useStore } from 'src/utils/StoreProvider'
 
 export interface ChatMessageProp {
   message: Message
@@ -10,7 +11,16 @@ export interface ChatMessageProp {
 
 export const ChatMessage = ({ message }: ChatMessageProp): JSX.Element => {
   const theme = useTheme()
-  return <Box className={`${styles.chat_message} ${message.mine ? styles.my_message : styles.roommate_message}`}>
+  const { themeStore } = useStore()
+
+  return <Box className={`${styles.chat_message} ${message.mine ? styles.my_message : styles.roommate_message}`}
+    sx={{
+      backgroundColor: themeStore.theme === 'light'
+      // Надо подумать куда добавить этот цвет
+        ? 'lightgreen'
+        : theme.palette.background.paper
+    }}
+  >
     <DoneAllIcon fontSize='small' className={styles.chat_message_status}
       sx={{
         color: message.read
