4ae6b8b33e1d523c4eec65b990aa55af7f3d61eb
wrongangel <wrongangel@gmail.com>
2023-08-14 16:36:07 +0300

    isFlipped problem

--- src/components/Cards/SearchCardController/SearchCardController.tsx
+++ src/components/Cards/SearchCardController/SearchCardController.tsx
@@ -1,21 +1,15 @@
 import { type MatchNew } from 'models'
-import ReactCardFlip from 'react-card-flip'
 import CardProfile from '../CardProfile/CardProfile'
-import CardApartment from '../CardApartment/CardApartment'
 import { useEffect, useState } from 'react'
 import styles from './SearchCardController.module.scss'
 import { Box } from '@mui/material'
+import CardDualPA from '../CardDualPA/CardDualPA'
 
 interface Props {
   matchNew: MatchNew
 }
 const SearchCardController = ({ matchNew }: Props): JSX.Element => {
-  const [isFlipped, setIsFlipped] = useState<boolean>(false)
   const [matches, setMatches] = useState<MatchNew[]>([])
-
-  const handleFlip = (): void => {
-    setIsFlipped(!isFlipped)
-  }
 
   const switchCards = (): void => {
     setTimeout(() => { setMatches([matches[0]]) }, 300)
@@ -36,13 +30,9 @@ const SearchCardController = ({ matchNew }: Props): JSX.Element => {
     <Box className={styles.card}>
       {matches.map((match, index) => (
         match.form.apartment?.id
-          ? <ReactCardFlip
-            key={match.user.id}
-            isFlipped={isFlipped}
-            containerClassName={`${styles.card__container} ${index === 1 && styles.card__container_right}`}>
-            <CardProfile info={match.form} person={match.user} padding='3rem' flipCard={handleFlip} />
-            <CardApartment apartment={match.form.apartment} user={match.user} padding='3rem' flipCard={handleFlip} />
-          </ReactCardFlip>
+          ? <Box key={match.user.id} className={`${styles.card__container} ${index === 1 && styles.card__container_right}`}>
+            <CardDualPA match={match} />
+          </Box>
           : <Box
             key={match.user.id}
             className={`${styles.card__container} ${index === 1 && styles.card__container_right}`}>
