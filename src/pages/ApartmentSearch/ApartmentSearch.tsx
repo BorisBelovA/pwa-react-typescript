2e287e311e592bdbc77c8bd20fe923fee41cb762
wrongangel <wrongangel@gmail.com>
2023-10-18 11:02:14 +0300

    Error show in apartment serch

--- src/pages/ApartmentSearch/ApartmentSearch.tsx
+++ src/pages/ApartmentSearch/ApartmentSearch.tsx
@@ -6,29 +6,57 @@ import { useStore } from 'src/utils/StoreProvider'
 import { FilterAltOutlined } from '@mui/icons-material'
 import { useNavigate } from 'react-router-dom'
 import { observer } from 'mobx-react-lite'
+import { useMainContext } from 'src/layouts/Main/MainLayout'
 
 const ApartmentSearch: React.FunctionComponent = () => {
   const { apartmentFiltersStore, apartmentSearchStore } = useStore()
+  const { setMessage } = useMainContext()
   const navigate = useNavigate()
 
   useEffect(() => {
     if (apartmentSearchStore.apartments.length < 1) {
       apartmentFiltersStore.getFromLocalStorage()
       apartmentFiltersStore.setPage(0)
-      void apartmentSearchStore.getApartments()
+      try {
+        apartmentSearchStore.getApartments()
+      } catch (error) {
+        setMessage({
+          text: error instanceof Error
+            ? error.message
+            : 'Something went wrong',
+          severity: 'error',
+          life: 5000,
+          visible: true
+        })
+      }
     }
     const root = document.getElementById('root')
     const handleScroll = () => {
       apartmentSearchStore.setScroll(root?.scrollTop || 0)
-    };
+    }
     root?.addEventListener('scroll', handleScroll)
     if (apartmentSearchStore.scroll > 0) {
       root?.scroll({top: apartmentSearchStore.scroll})
     }
     return () => {
-      root?.removeEventListener('scroll', handleScroll);
-    };
+      root?.removeEventListener('scroll', handleScroll)
+    }
   }, [])
+
+    const getMore = async (): Promise<void> => {
+      try {
+        apartmentSearchStore.getMoreApartments()
+      } catch (error) {
+        setMessage({
+          text: error instanceof Error
+            ? error.message
+            : 'Something went wrong',
+          severity: 'error',
+          life: 5000,
+          visible: true
+        })
+      }
+    }
 
   return (
     <Box className={styles.householdContainer}>
@@ -43,11 +71,11 @@ const ApartmentSearch: React.FunctionComponent = () => {
       </Box>
       {apartmentSearchStore.apartments.map((apartment) => <ApartmentThumbnail apartment={apartment} key={apartment.id} />)}
       {apartmentSearchStore.haveMore
-        ? <Button variant='contained' onClick={() => { void apartmentSearchStore.getMoreApartments() }}>Load more</Button>
+        ? <Button variant='contained' onClick={() => { getMore() }}>Load more</Button>
         : <Box className={styles.household__noMore}>
           <Typography variant='h2'>Sorry, can&apos;t find more apartments</Typography>
           <Button variant='contained' onClick={() => { navigate('filters') }}>Change filters</Button>
-          <Button variant='outlined' onClick={() => { void apartmentSearchStore.getMoreApartments() }}>Try again</Button>
+          <Button variant='outlined' onClick={() => { getMore() }}>Try again</Button>
         </Box>
       }
     </Box>
