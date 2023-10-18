eefdb52c3c25fa30a0d66633276f07d1aa7071ec
wrongangel <wrongangel@gmail.com>
2023-10-18 10:40:14 +0300

    Yandex metrika

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,11 @@ import { configure } from 'mobx'
 import CustomThemeProvider from './styles/CustomThemeProvider'
 import { type IBeforeInstallPromptEvent, PromptToInstall } from './context/promptToInstall'
 import ReactGA from 'react-ga4'
+import { YMInitializer } from '@appigram/react-yandex-metrika'
 
 configure({ enforceActions: 'always' })
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize('G-9HTPPGN44N')
 }
 
@@ -43,12 +44,22 @@ const App = (): JSX.Element => {
   }, [])
 
   return <React.StrictMode>
+    {process.env.NODE_ENV === 'production' &&
+      <YMInitializer
+        accounts={[95287962]}
+        options={{
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true
+        }}
+      />
+    }
     <BrowserRouter basename="/">
       <StoreProvider store={store}>
-        <PromptToInstall.Provider value={{deferredEvt, hidePrompt}}> 
+        <PromptToInstall.Provider value={{ deferredEvt, hidePrompt }}>
           <CustomThemeProvider>
             <CssBaseline />
-              <Router />
+            <Router />
           </CustomThemeProvider>
         </PromptToInstall.Provider>
       </StoreProvider>
